fbe681886cfe7c7b877933a77851630291f870f1
Oluwatofunmi Caulcrick <oluwatofunmicaulcrick@Oluwatofunmis-MacBook-Air.local>
2025-08-10 12:46:46 +0100

    New randomfile.txt added & fourth feature

--- to_distributions/example_code.py
+++ to_distributions/example_code.py
@@ -2,5 +2,6 @@ from to_distributions.gaussiandistribution import Gaussian
 #Another feature
 #One more feature
 #Third feature
+#Fourth feature
 gaussian_one = Gaussian(22, 2)
 print(gaussian_one.mean)
